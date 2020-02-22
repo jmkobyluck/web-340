e23e4c4fcbd5b9830840b99402d7a50e96c49a9d
Jonathan Koblyuck <jkobyluck@my365.bellevue.edu>
2020-02-22 13:07:05 -0500

    Adding Exercise 1.3

---
+++ week-1/kobyluck-exercise-1.3.js
@@ -0,0 +1,32 @@
+/*
+============================================
+; Title:  Exercise 1.3
+; Author: Professor Krasso
+; Date:   22 February 2020
+; Modified By: Jonathan Kobyluck
+; Description: Modules
+;===========================================
+*/
+
+// Require statement that imports my header file
+const header = require('../kobyluck-header.js');
+
+// Calling the console log function to display my header
+console.log(header.display('Johnny', 'Kobyluck', 'Exercise 1.3'));
+
+// line break
+console.log("");
+
+// start program
+
+// declaring url variable
+var url = require('url');
+
+var parsedURL = url.parse('https://www.exercise1point3.com/profile?name=kobyluck');
+
+// outputting the url protocol, host, and querys
+console.log(parsedURL.protocol);
+console.log(parsedURL.host);
+console.log(parsedURL.query);
+
+// end program
