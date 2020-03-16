0537d1679ad52af3276e324cadeb346962c4b3da
Jonathan Koblyuck <jkobyluck@my365.bellevue.edu>
2020-03-15 21:41:31 -0400

    updating comments

--- week-4/json-api/kobyluck-exercise-4.2.js
+++ week-4/json-api/kobyluck-exercise-4.2.js
@@ -1,10 +1,33 @@
+/*
+;============================================
+; Title: Exercise 4.2
+; Author: Professor Krasso
+; Date: 15 March 2020
+; Modified By: Jonathan Kobyluck
+; Description: JSON APIs
+;===========================================
+*/
+
+// Require statement that imports my header file
+const header = require('../..kobyluck-header');
+
+// Calling the console log function to display my header
+console.log(header.display('Johnny', 'Kobyluck', 'Exercise 4.2'));
+
+// Line break
+console.log(" ")
+
+
+// start program
+
+// require statements
 var express = require('express');
 var http = require('http');
 
-
+// express declaration
 var app =express();
 
-
+// .get function to retrieve information from api
 app.get("/customer/:id", function(req, res) {
   var id = parseInt(req.params.id, 10);
   res.json({
@@ -14,6 +37,7 @@ app.get("/customer/:id", function(req, res) {
   });
 });
 
+// creating the server on port 8080
 http.createServer(app).listen(8080, function() {
   console.log("Application has started on port %s", 8080);
 });
--- week-4/status-codes/kobyluck-exercise-4.3.js
+++ week-4/status-codes/kobyluck-exercise-4.3.js
@@ -1,9 +1,34 @@
+/*
+;============================================
+; Title: Exercise 4.2
+; Author: Professor Krasso
+; Date: 15 March 2020
+; Modified By: Jonathan Kobyluck
+; Description: JSON APIs
+;===========================================
+*/
+
+// Require statement that imports my header file
+const header = require('../..kobyluck-header');
+
+// Calling the console log function to display my header
+console.log(header.display('Johnny', 'Kobyluck', 'Exercise 4.2'));
+
+// Line break
+console.log(" ")
+
+
+// start program
+
+// require statements
 var express = require('express');
 var http = require('http');
 var logger = require('morgan');
 
+// express declaration
 var app = express();
 
+// using morgan for logger
 app.use(logger('dev'));
 
 
@@ -32,6 +57,7 @@ app.get('/not-implemented', function(req, res) {
   });
 });
 
+// creating server on port 8080
 http.createServer(app).listen(8080, function() {
   console.log('Application has started on port %s', 8080);
 });
