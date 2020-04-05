3db30b67005dba7ab6c0f180a74649242c3c677c
Jonathan Koblyuck <jkobyluck@my365.bellevue.edu>
2020-04-05 14:52:15 -0400

    Adding Assignment 7.4

--- ems/app.js
+++ ems/app.js
@@ -1,8 +1,38 @@
+/*
+============================================
+; Title: Assignment 6.4
+; Author: Professor Krasso
+; Date: 29 March 2020
+; Modified By: Jonathan Kobyluck
+; Description:
+;===========================================
+*/
+
 var express = require("express");
 var http = require("http");
 var path = require("path");
+var mongoose = require("mongoose");
+
 var logger = require("morgan");
 
+var Employee = require("./models/employee")
+
+var mongoDB = "<mLab connection string>";
+
+mongoose.connect(mongoDB, {
+  useMongoClient: true
+});
+
+mongoose.Promise = global.Promise;
+
+var db = mongoose.connection;
+
+db.on("error", console.error.bind(console, "MongoDB connection error: "));
+db.once("open", function() {
+  console.log("Application connected to mLab MongoDB instance");
+})
+
+// application
 var app = express();
 
 app.set("views", path.resolve(__dirname, "views"));
@@ -11,12 +41,18 @@ app.set("view engine", "ejs");
 app.use(logger("short"));
 app.use(express.static(__dirname+'/public'));
 
+// model
+var employee = new Employee({
+  name:"Tony Stark"
+})
+
 app.get("/", function(req, res) {
   res.render("index", {
     title: "Employee Management System"
   });
 });
 
+// create server
 http.createServer(app).listen(8080, function() {
   console.log("Application has started on port %s", 8080);
 });
---
+++ ems/models/employee.js
@@ -0,0 +1,21 @@
+/*
+============================================
+; Title: Assignment 7.4
+; Author: Professor Krasso
+; Date: 5 April 2020
+; Modified By: Jonathan Kobyluck
+; Description: Mongoose Schema
+; and Model Development
+;===========================================
+*/
+
+var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
+
+// define employeeSchema
+var employeeSchema = new Schema({
+  name: {type: String, required: true}
+});
+
+// define the employee model
+module.exports = mongoose.model("Employee", employeeSchema);
