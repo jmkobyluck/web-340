f401e1d547209dec846765f5341f3abdc3f8a54f
Jonathan Koblyuck <jkobyluck@my365.bellevue.edu>
2020-04-09 15:06:08 -0400

    Updated file for Exercise 8.3

--- ems/app.js
+++ ems/app.js
@@ -1,10 +1,10 @@
 /*
 ============================================
-; Title: Assignment 6.4
+; Title: Exercise 8.2
 ; Author: Professor Krasso
-; Date: 29 March 2020
+; Date: 9 April 2020
 ; Modified By: Jonathan Kobyluck
-; Description:
+; Description: Cross-Site Scripting
 ;===========================================
 */
 
@@ -12,15 +12,16 @@ var express = require("express");
 var http = require("http");
 var path = require("path");
 var mongoose = require("mongoose");
+var helmet = require("helmet");
 
 var logger = require("morgan");
 
-var Employee = require("./models/employee")
+var Employee = require("./models/employee");
 
 var mongoDB = "<mLab connection string>";
 
 mongoose.connect(mongoDB, {
-  useMongoClient: true
+  useMongoClient: true,
 });
 
 mongoose.Promise = global.Promise;
@@ -28,31 +29,32 @@ mongoose.Promise = global.Promise;
 var db = mongoose.connection;
 
 db.on("error", console.error.bind(console, "MongoDB connection error: "));
-db.once("open", function() {
+db.once("open", function () {
   console.log("Application connected to mLab MongoDB instance");
-})
+});
 
 // application
 var app = express();
 
+app.use(logger("short"));
+app.use(express.static(__dirname + "/public"));
+app.use(helmet.xssFilter());
+
 app.set("views", path.resolve(__dirname, "views"));
 app.set("view engine", "ejs");
 
-app.use(logger("short"));
-app.use(express.static(__dirname+'/public'));
-
 // model
 var employee = new Employee({
-  name:"Tony Stark"
-})
+  name: "Tony Stark",
+});
 
-app.get("/", function(req, res) {
+app.get("/", function (req, res) {
   res.render("index", {
-    title: "Employee Management System"
+    title: "Employee Management System",
   });
 });
 
 // create server
-http.createServer(app).listen(8080, function() {
+http.createServer(app).listen(8080, function () {
   console.log("Application has started on port %s", 8080);
 });
