b396797e07a9baf70ca5bf7dc565a2d42c7d391f
Jonathan Koblyuck <jkobyluck@my365.bellevue.edu>
2020-04-13 01:05:34 -0400

    Update

--- week-2/ejs-views/kobyluck-assignment-2.4.js
+++ week-2/ejs-views/kobyluck-assignment-2.4.js
@@ -9,10 +9,10 @@
 */
 
 // Require statement that imports my header file
-const header = require('../../kobyluck-header.js');
+const header = require("../../kobyluck-header.js");
 
 // Calling the console log function to display my header
-console.log(header.display('Johnny', 'Kobyluck', 'Assignment 2.4'));
+console.log(header.display("Johnny", "Kobyluck", "Assignment 2.4"));
 
 // line break
 console.log("");
@@ -29,9 +29,9 @@ Application started on port
 */
 
 // require statements
-var express = require('express');
-var http = require('http');
-var path = require('path');
+var express = require("express");
+var http = require("http");
+var path = require("path");
 
 // calling express to run a new express application
 var app = express();
@@ -40,15 +40,15 @@ app.set("views", path.resolve(__dirname, "views"));
 app.set("view engine", "ejs");
 
 // routes
-app.get("/", function(req, res) {
+app.get("/", function (req, res) {
   response.render("index", {
     firstName: "Jonathan",
     lastName: "Kobyluck",
-    address: "123 Webdev Ln"
+    address: "123 Webdev Ln",
   });
 });
 
 // starting the server on port 8080
-http.createServer(app).listen(8080, function() {
+http.createServer(app).listen(8080, function () {
   console.log("EJS-Views app started on port %s", 8080);
 });
